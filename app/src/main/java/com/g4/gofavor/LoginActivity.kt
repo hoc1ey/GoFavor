bbd002d7310d8de1a6bd1da75a2ae76f721a93f8
hocley <jose.merchan01@epn.edu.ec>
2025-06-12 19:45:09 -0500

    code(master): Navegavility between LoginActivity.kt, RegisterActivity.kt and MainActivity added.

--- app/src/main/java/com/g4/gofavor/LoginActivity.kt
+++ app/src/main/java/com/g4/gofavor/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.g4.gofavor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var buttonLogin : Button
+    private lateinit var buttonRegister : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,29 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initUI()
+
+    }
+
+    private fun initUI() {
+        initUIComponents()
+        initEventListeners()
+    }
+
+    private fun initEventListeners() {
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initUIComponents() {
+        buttonLogin = findViewById(R.id.buttonLogin)
+        buttonRegister = findViewById(R.id.buttonRegister)
     }
 }
