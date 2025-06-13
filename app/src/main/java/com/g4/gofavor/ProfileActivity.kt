29e999cf3be7fbd8ca6c9de8206a3c350ea3242c
Andrés Mateo Quilupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-06-12 20:28:53 -0500

    code(master): Navigability of ProfileActivity.kt

--- app/src/main/java/com/g4/gofavor/ProfileActivity.kt
+++ app/src/main/java/com/g4/gofavor/ProfileActivity.kt
@@ -1,12 +1,18 @@
 package com.g4.gofavor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var buttonReturn: Button
+    private lateinit var buttonLogout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,27 @@ class ProfileActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIComponents()
+        initEventListeners()
+    }
+
+    private fun initUIComponents() {
+        buttonReturn = findViewById(R.id.buttonReturn)
+        buttonLogout = findViewById(R.id.buttonLogout)
+    }
+
+    private fun initEventListeners() {
+        buttonReturn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        buttonLogout.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
