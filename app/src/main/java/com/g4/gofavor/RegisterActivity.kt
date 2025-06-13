6f0780850d9bb0c075dd0677eb772290ed03a5be
Andrés Mateo Quilupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-06-12 19:46:04 -0500

    code(master): Navigability between RegisterActivity.kt and LoginActivity.kt

--- app/src/main/java/com/g4/gofavor/RegisterActivity.kt
+++ app/src/main/java/com/g4/gofavor/RegisterActivity.kt
@@ -1,12 +1,17 @@
 package com.g4.gofavor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var buttonRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,23 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIComponents()
+        initEventListeners()
+    }
+
+    private fun initUIComponents() {
+        buttonRegister = findViewById(R.id.buttonRegister)
+    }
+
+    private fun initEventListeners() {
+        buttonRegister.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
