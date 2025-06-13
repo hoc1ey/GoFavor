56486d3fec41681b99cc1f28efef7e9c7d1295d8
Andrés Mateo Quilupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-06-12 22:37:56 -0500

    code(master): Verification of navigability of the app

--- app/src/main/java/com/g4/gofavor/NotificationsActivity.kt
+++ app/src/main/java/com/g4/gofavor/NotificationsActivity.kt
@@ -1,12 +1,17 @@
 package com.g4.gofavor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class NotificationsActivity : AppCompatActivity() {
+
+    private lateinit var ButtonReturn: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,24 @@ class NotificationsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIComponents()
+        initEventListeners()
+    }
+
+    private fun initUIComponents() {
+        ButtonReturn = findViewById(R.id.buttonReturn)
+    }
+
+    private fun initEventListeners() {
+        ButtonReturn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
